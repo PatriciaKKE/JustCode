d15d1dc3f5f85f9813cfb511b427fc6e78aae3c5
Joseph P. <joseph@email.com>
2025-04-23 00:16:57 +0200

    controleur de base

--- public/index.php
+++ public/index.php
@@ -1,9 +1,17 @@
 <?php
+namespace App\Controller;
 
-use App\Kernel;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            
+        ]);
+    }
+}
